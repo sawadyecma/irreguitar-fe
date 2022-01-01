d2c9d0072f6199b37188c164e60f2ddd0067a374
sawadyecma <snowslidez@yahoo.co.jp>
2022-01-01 14:14:30 +0900

    add Thead Box Stories

--- src/components/ThreadBox.stories.tsx
+++ src/components/ThreadBox.stories.tsx
@@ -6,13 +6,41 @@ export default {
     component: ThreadBox,
 };
 
-export const OctaveTheadPressed = () => <ThreadBox fletCnt={12} markedFlets={[3]}/>;
-export const OctaveThreadOpened = () => <ThreadBox fletCnt={12} />;
-export const OnePressableOctaveThread = () => {
-    const [pressedFlet, setPressedFlet] = useState<number>(0)
-    return <ThreadBox 
-        fletCnt={12} 
-        markedFlets={[pressedFlet]}
-        onClick={(f)=>setPressedFlet(f)}
+export const NotPressed = () => <ThreadBox markedFlets={[]}/>;
+export const Pressed = () => <ThreadBox markedFlets={[3]}/>;
+export const Opened = () => <ThreadBox markedFlets={[0]}/>;
+
+export const SinglePressable = () => {
+    const [pressedFlet, setPressedFlet] = useState<number|undefined>(undefined)
+    
+    const onClickHandler = (fletNum: number): void => {
+        if(fletNum === pressedFlet){
+            setPressedFlet(undefined)
+            return
+        }
+        setPressedFlet(fletNum)
+    }
+
+    return <ThreadBox
+        markedFlets={pressedFlet === undefined ? [] : [pressedFlet]}
+        onClick={onClickHandler}
+    />
+}
+
+export const MultiPressable = () => {
+    const [pressedFlets, setPressedFlets] = useState<number[]>([])
+    
+    const onClickHandler = (fletNum: number): void => {
+        if (pressedFlets.includes(fletNum)){
+            setPressedFlets(pressedFlets.filter((f)=>f!==fletNum))
+            return
+        }
+        
+        setPressedFlets((d)=>[...d, fletNum])
+    }
+
+    return <ThreadBox
+        markedFlets={pressedFlets}
+        onClick={onClickHandler}
     />
 }
