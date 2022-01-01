350a0c9086e4c939725dc2ab8f1de154e86c9b00
sawadyecma <snowslidez@yahoo.co.jp>
2022-01-01 13:54:52 +0900

    add thread color

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,4 +1,4 @@
-import { CELL_COLOR, CELL_HEIGHT, CELL_WEIGHT, CELL_WIDTH, PRESS_CIRCLE_RADIUS } from "../constants/cell";
+import { CELL_COLOR, CELL_HEIGHT, CELL_WEIGHT, CELL_WIDTH, PRESS_CIRCLE_COLOR, PRESS_CIRCLE_RADIUS, THREAD_COLOR, THREAD_WEIGHT } from "../constants/cell";
 
 interface Props {
     pressed?: Boolean
@@ -12,20 +12,27 @@ export const Cell = ({
 
     const w = CELL_WIDTH;
     const h = CELL_HEIGHT;
-    const sw = CELL_WEIGHT
-    const sc = CELL_COLOR
-    const circleRadius = PRESS_CIRCLE_RADIUS
+    const cw = CELL_WEIGHT;
+    const cc = CELL_COLOR;
+
+    const tw = THREAD_WEIGHT;
+    const tc = THREAD_COLOR;
+
+    const pr = PRESS_CIRCLE_RADIUS;
+    const pc = PRESS_CIRCLE_COLOR;
 
     return (
         <svg width={w} height={h} onClick={onClick}>
             {/* 上横線 */}
-            <line x1={0} y1={0} x2={w} y2={0} strokeWidth={sw} stroke={sc}/>
+            <line x1={0} y1={0} x2={w} y2={0} strokeWidth={cw} stroke={cc}/>
             {/* 下横線 */}
-            <line x1={0} y1={h} x2={w} y2={h} strokeWidth={sw} stroke={sc}/>
-            {/* 中縦線 */}
-            <line x1={w/2} y1={0} x2={w/2} y2={h} strokeWidth={sw/2} stroke={sc}/>
+            <line x1={0} y1={h} x2={w} y2={h} strokeWidth={cw} stroke={cc}/>
+            {/* 中縦線 = フレット */}
+            <line x1={w/2} y1={0} x2={w/2} y2={h} strokeWidth={tw} stroke={tc}/>
+            {/* 中横線 = 弦 */}
+            <line x1={0} y1={h/2} x2={w} y2={h/2} strokeWidth={tw} stroke={tc}/>
             {pressed && 
-                <circle cx={w/2} cy={h/2} r={circleRadius} strokeWidth={sw} stroke={sc} fill={sc}/>
+                <circle cx={w/2} cy={h/2} r={pr} strokeWidth={cw} stroke={pc} fill={pc}/>
             }
         </svg>
     )
