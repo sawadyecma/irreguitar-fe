4eea026fdf8f3fd70062bf132cf14a56e4db1e27
sawadyecma <snowslidez@yahoo.co.jp>
2022-01-05 09:18:26 +0900

    install irreguitar-pkg

--- src/pages/TurningList/TurningList.tsx
+++ src/pages/TurningList/TurningList.tsx
@@ -1,13 +1,27 @@
 import { Cell } from "../../components/svg/Cell"
-
+import { AbsnoteImpl, Absnotes, ChordAnalyzerImpl, ChordImpl, ChordNamerImpl, ChordParserImpl } from "irreguitar-pkg"
 export const TurningList = () => {
     const flets = Array.from(Array(12), (_, k) => k)
+
+    console.log(new AbsnoteImpl(Absnotes.A1))
+
+    const chord = new ChordImpl(
+        new AbsnoteImpl(Absnotes.C3),
+        [
+            new AbsnoteImpl(Absnotes.E3),
+            new AbsnoteImpl(Absnotes.G3),
+        ]
+    )
+
+    const parser = new ChordParserImpl(new ChordAnalyzerImpl(), new ChordNamerImpl())
+
     return (
         <>
         <div>TurningList</div>
         {flets.map((f) => {
             return <Cell marked={f%3===2}/>
         })}
+        chord name: {parser.parse(chord)}
         </>
     )
 }
