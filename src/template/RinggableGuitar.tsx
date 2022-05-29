6ca99a1aff8c6d8579acd82c0f08ae218089883b
sawadyecma <snowslidez@yahoo.co.jp>
2022-05-29 17:35:46 +0900

    add onStroke at RinggableGuitar

--- src/template/RinggableGuitar.tsx
+++ src/template/RinggableGuitar.tsx
@@ -2,6 +2,7 @@ import { AbsnoteImpl, ThreadImpl, TURNING_CATALOG } from "irreguitar-pkg";
 import { GuitarBox } from "../components/GuitarBox";
 import { ThreadsFactory } from "../types/thread";
 import * as Tone from "tone";
+import { Button } from "antd";
 
 export const RinggableGuitar = () => {
   const threads = ThreadsFactory.create({ turning: TURNING_CATALOG.Regular });
@@ -21,5 +22,32 @@ export const RinggableGuitar = () => {
     synth.triggerAttackRelease(note.getName(), "32n", now);
   };
 
-  return <GuitarBox threads={threads} onClick={onClickHandler} />;
+  const onStroke = (stroke: "up" | "down") => {
+    const absnotes = threads
+      .map((thread) => new AbsnoteImpl(thread.openNote))
+      .sort((a, b) =>
+        stroke === "down"
+          ? a.getValue() - b.getValue()
+          : b.getValue() - a.getValue()
+      )
+      .map((absnote) => absnote.getName());
+
+    const synth = new Tone.PolySynth(Tone.Synth).toDestination();
+    const now = Tone.now();
+    absnotes.forEach((absnote, index) => {
+      synth.triggerAttackRelease(absnote, "8n", now + 0.03 * index);
+    });
+  };
+
+  return (
+    <>
+      <GuitarBox threads={threads} onClick={onClickHandler} />
+      <Button type="primary" onClick={() => onStroke("down")}>
+        Down Stroke
+      </Button>
+      <Button type="primary" onClick={() => onStroke("up")}>
+        Up Stroke
+      </Button>
+    </>
+  );
 };
